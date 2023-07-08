fe56100ee57de363efb2a4d84e67621cd4cf8c67
Aafreen-Afi <137905884+Aafreen-Afi@users.noreply.github.com>
2023-07-08 22:07:35 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,108 @@
+"""
+1. Print Statement
+Output: HOPE AI
+"""
+print("HOPE AI")
+
+"""
+2. Buy input
+Output: Enter your institute name? : HOPE AI
+"""
+in_name=input("Enter you institute name? : ")
+
+"""
+3. Buy inputs
+Output:
+Enter your name: Raj
+Enter your age: 15
+Enter your school name: Guru school
+Enter your degree: B.E CSE
+"""
+name=input("Enter your name: ")
+age=input("Enter your age: ") 
+sh_name=input("Enter your school name: ")
+degree=input("Enter your degree: ")
+
+"""
+4. Addition
+Output:
+a= 43
+b= 34
+add = 77
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+add=a+b
+print("add = ",add)
+
+"""
+5. Subtraction
+Output:
+a= 43
+b= 34
+Sub = 9
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+Sub=a-b
+print("Sub = ",Sub)
+
+"""
+6. Multiplication
+Output:
+a= 63
+b= 97
+Mul = 6111
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+Mul=a*b
+print("Mul = ",Mul)
+
+"""
+7. Division
+Output:
+a= 76
+b= 31
+Float Div = 2.4516129032258065
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+div=a/b
+print("Float Div = ",div)
+
+"""
+8. Floor division
+Output:
+a= 76
+b= 31
+Floor Div = 2
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+div=a//b
+print("Floor Div = ",div)
+
+"""
+9. Modulo
+Output:
+a= 76
+b= 31
+Modulo = 14
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+div=a%b
+print("Modulo = ",div)
+
+"""
+10. Power
+Output:
+a= 4
+b= 4
+Power = 256
+"""
+a=int(input("a= "))
+b=int(input("b= "))
+power=a**b
+print("Power = ",power)
